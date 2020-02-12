3df246297248c88f2a5e8a6b718d684d759a8e05
Alan Jordan <alan.jordanbiz@gmail.com>
2020-02-12 10:52:24 -0500

    initial commit

---
+++ js/get.js
@@ -0,0 +1,31 @@
+`use strick`;
+
+function get(url) {
+    return fetch(url)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        return data;
+    })
+    .catch(function(error) {
+        return error;
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+// // const get = function(url){
+//     return fetch history(url)
+//     .then(response => response.json())
+//     .then(data => data);
+// // }
+//(someArgunments) => {someFunctionReturn() }
