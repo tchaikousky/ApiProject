57a6c6dc24a38c1ac9c47488324835d95a695943
Tchaikousky Thomas <tchaikouskythomas@gmail.com>
2020-02-13 16:02:49 -0500

    began implementing the getSingleItem Function

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,17 @@
 `use scrict`;
 
 const apiKey = `f218c55bc041494885c415afbc00faaa`
+const restuarants = [];
 
-function getRestaurants() {
-    let restuarantName = `Hooters`;
+function getRestaurant(restaurantName) {
     
     
-
-    
-
 }
 
-function getMenuItem(menuItem) {
+function getMenuItems() {
     const apiUrl = `https://api.spoonacular.com/food/menuItems/search?query=Hooters&number=60&apiKey=f218c55bc041494885c415afbc00faaa`;
     
-    get(apiUrl).then(function(response) {
+    const l = get(apiUrl).then(function(response) {
         const restaurantMenuList = response.menuItems.filter(function(menuItem) {
             console.log(response.menuItems[0].restaurantChain)
             if(menuItem.restaurantChain === "Hooters") {
@@ -24,11 +21,30 @@ function getMenuItem(menuItem) {
         console.log(restaurantMenuList);
         // console.log(restaurantList);
         // console.log(response.value);
+        return restaurantMenuList;
     });
+
+    return l;
+
+}
+
+function getNutritionInfo(restaurantChain) {
+    // get(apiUrl).then(function(response) {
+    //     const menuItemId = 
+}
+
+function getSingleMenuItem(itemName) {
+    const menuItemId = 0;
+    restaurantMenu.forEach(menuItem => {
+        if(menuItem.title === itemName) {
+            menuItemId = menuItem.id;
+        }
+    });
+    return menuItemId;
 }
 
 
-getMenuItem();
-
+// console.log(getSingleMenuItem("Caesar Salad"));
+console.log(getMenuItems());
 
 // /search?query=${restuarantName}&number=60&apiKey=${apiKey}
