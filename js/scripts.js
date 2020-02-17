abeeec43414f629dde4087c33b80fd8f9b619b0f
Tchaikousky Thomas <tchaikouskythomas@gmail.com>
2020-02-17 13:55:03 -0500

    added images to page2's modal

--- js/scripts.js
+++ js/scripts.js
@@ -26,37 +26,7 @@ closeModalButton.addEventListener("click", function(e) {
 logos.addEventListener("click", function(e){
     document.cookie = e.target.value;
     if (e.target.value != undefined) {
-        location.assign(`page2.html`);
-        // if(e.target.value === "Burger King") {
-        //     location.assign(`page2.html`);
-        // }
-        // else if(e.target.value === "Carl's Jr") {
-        //     location.assign("page2.html");
-        // }
-        // else if(e.target.value === "Hooter's") {
-        //     location.assign("page2.html");
-        // }
-        // else if(e.target.value === "Jack In The Box") {
-        //     location.assign("page2.html");
-        // }
-        // else if(e.target.value === "Sonic") {
-        //     location.assign("page2.html");
-        // }
-        // else if(e.target.value === "Wendy's") {
-        //     location.assign("page2.html");
-        // }
-        // else if(e.target.value === "McDonald's") {
-        //     location.assign("page2.html");
-        // }
-        // else if(e.target.value === "Bojangles") {
-        //     location.assign("page2.html");
-        // }   
+        location.assign(`page2.html`);   
     }
     return e.target.value;   
 });
-
-// console.log(getNutritionInfo(419330));
-// console.log(getCaloriesBurned(60, 215, getActivity("casual walking")));
-// console.log(getActivity("house cleaning"));
-// console.log(convertLbToKg(215));
-// console.log(getTimeToBurnCalories(205, 215, getActivity("casual walking")));
